9f2c94172fd2cd36cae02298201f08b3922f3531
JoseRCL1981 <jribamarcl@hotmail.com>
2024-10-02 20:33:06 -0300

    Add files via upload

---
+++ Aula02.py
@@ -0,0 +1,25 @@
+#Soma de dois números
+a = 32
+b = 25
+print(a+b)
+
+
+#Nome de usuário
+primeiro_nome = 'José de Ribamar'
+sobrenome = 'Carvalho Lima'
+print("Exiba o nome completo:",primeiro_nome,sobrenome)
+
+#Média de  três notas
+n1 = 9
+n2 = 7
+n3 = 10
+media = n1+n2+n3
+media_final = (n1+n2+n3) /3
+print(';Calcule a média:',media)
+print("Mostre a média final:",media_final)
+
+#Área de um retângulo
+largura = 12
+altura = 9
+area = 12*9
+print('Calcule a Área de um retângulo:',area)
